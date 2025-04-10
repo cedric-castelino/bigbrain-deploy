1fb116a47e06cbc7369f637401e9671872d5a0a0
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-10 15:04:51 +1000

    added login basics, carbon copy of register

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,14 +1,43 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
+function Login () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+
+    const Login = async () => {
+      try {
+        const response = await axios.post('http://localhost:5005/admin/auth/login', {
+          email: email,
+          password: password
+        })
+  
+        const token = response.data.token;
+        localStorage.setItem('token', token)
+        console.log(token);
+        navigate('/dashboard')
+        
+      } catch (err) {
+        alert(err.response.data.error);
+      }
+  
+    }
   
     return (
-        <>
-            test
-        </>
+      <>
+        <section className="register-page">
+          <h1>Login</h1>
+          Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
+          password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
+          <button onClick={Login}>Login</button>
+        </section>
+      </>
     )
   }
-  
+
 export default Login;
