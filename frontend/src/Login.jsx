774861de007cc783167a973eb218a2c0dc7b1101
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-10 15:37:20 +1000

    fixing indent

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,35 +2,37 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Login ({ successJob }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const Login = async () => {
-      try {
-        const response = await axios.post('http://localhost:5005/admin/auth/login', {
-          email: email,
-          password: password
-        })
-  
-        const token = response.data.token;
-        successJob(token)
-        
-      } catch (err) {
-        alert(err.response.data.error);
-      }
-  
+
+
+  const Login = async () => {
+    try {
+      const response = await axios.post('http://localhost:5005/admin/auth/login', {
+        email: email,
+        password: password
+      })
+
+      const token = response.data.token;
+      successJob(token)
+      
+    } catch (err) {
+      alert(err.response.data.error);
     }
-  
-    return (
-      <>
-        <section className="register-page">
-          <h1>Login</h1>
-          Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
-          password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
-          <button onClick={Login}>Login</button>
-        </section>
-      </>
-    )
+
   }
+
+  return (
+    <>
+      <section className="register-page">
+        <h1>Login</h1>
+        Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
+        password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
+        <button onClick={Login}>Login</button>
+      </section>
+    </>
+  )
+}
 
 export default Login;
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -2,35 +2,35 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Register({ successJob }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const register = async () => {
-      try {
-        const response = await axios.post('http://localhost:5005/admin/auth/register', {
-          email: email,
-          password: password
-        })
-  
-        const token = response.data.token;
-        successJob(token)
-        
-      } catch (err) {
-        alert(err.response.data.error);
-      }
-  
+  const register = async () => {
+    try {
+      const response = await axios.post('http://localhost:5005/admin/auth/register', {
+        email: email,
+        password: password
+      })
+
+      const token = response.data.token;
+      successJob(token)
+      
+    } catch (err) {
+      alert(err.response.data.error);
     }
-  
-    return (
-      <>
-        <section className="register-page">
-          <h1>Register</h1>
-          Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
-          password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
-          <button onClick={register}>Register</button>
-        </section>
-      </>
-    )
+
   }
+
+  return (
+    <>
+      <section className="register-page">
+        <h1>Register</h1>
+        Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
+        password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
+        <button onClick={register}>Register</button>
+      </section>
+    </>
+  )
+}
 
 export default Register;
