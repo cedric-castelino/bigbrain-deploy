c630e30d0ab4307abee426466c808af21edf333e
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-10 14:39:46 +1000

    added react router dom and create new fiels for cedric to do login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react';
 import axios from 'axios';
 
 import Register from './Register';
+import Login from './Login';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
   return (
     <>
-      <Register />
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
