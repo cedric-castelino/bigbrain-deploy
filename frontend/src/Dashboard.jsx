cce6e27c44eaa85a98e8dc9346b08ff6f137642b
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-11 12:39:19 +1000

    added game card and made a set games

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,15 +1,36 @@
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
 
 function Dashboard({ token }) {
+    const [games, setGames] = useState('');
     const navigate = useNavigate();
-    console.log(token)
+    
     if (!token) {
         navigate('/login');
     };
+
+
+  const getDashboardGames = async () => {
+    try {
+      const response = await axios.get('http://localhost:5005/admin/games', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      })
+      setGames(response.data.games)
+    } catch (err) {
+        alert(err.response.data.error);
+    }
+  }
     
     return (
         <>
-            Hi
+            {
+
+            }
+            <Button onClick={getDashboardGames} variant='primary'>Register</Button>
         </>
     )
 }
