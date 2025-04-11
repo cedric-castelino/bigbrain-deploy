2277aa8c8817f2afafa7489bf08e98c26ebd2c33
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-11 12:08:30 +1000

    adding conditions for register

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -7,25 +7,41 @@ import Button from 'react-bootstrap/Button'
 function Register({ successJob, token}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
   const navigate = useNavigate();
 
   if (token) {
     navigate('/dashboard');
   };
 
-  const register = async () => {
-    try {
-      const response = await axios.post('http://localhost:5005/admin/auth/register', {
-        email: email,
-        password: password
-      })
+  function checkEmailValidity (event) {
+    const emailRegex = /^.+\@.+\..+$/;
+    let emailValid = false;
+    emailValid = emailRegex.test(event); // check if the email is in the correct form
+    return emailValid;
+  };
 
-      const token = response.data.token;
-      successJob(token)
-      
-    } catch (err) {
-      alert(err.response.data.error);
+  const register = async () => {
+
+    if (confirmPassword !== password || password === '') {
+      alert("Passwords are not the same");
+    } else if (!checkEmailValidity(email)) {
+      alert("Email isn't correct");
+    } else {
+        try {
+          const response = await axios.post('http://localhost:5005/admin/auth/register', {
+            email: email,
+            password: password
+          })
+
+          const token = response.data.token;
+          successJob(token)
+          
+        } catch (err) {
+          alert(err.response.data.error);
+        }      
     }
+
 
   }
 
@@ -34,7 +50,8 @@ function Register({ successJob, token}) {
       <section className="register-page">
         <h1>Register</h1>
         Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
-        password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
+        password: <input value={password} onChange={e => setPassword(e.target.value)} type="password"/> <br/> 
+        password: <input value={confirmPassword} onChange={e => setconfirmPassword(e.target.value)} type="password"/> <br/> 
         <Button onClick={register} variant='primary'>Register</Button>
       </section>
     </>
