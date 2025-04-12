8f90aa14e910fd7ee949bd80269939e63b8d75c9
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-13 00:15:50 +1000

    changed the height of delete modal to 45vh not 50vh. moved the close buttons on create and delete modals to the bottom

--- frontend/components/DeleteModal.jsx
+++ frontend/components/DeleteModal.jsx
@@ -3,15 +3,15 @@ function DeleteModal ({open, onClose, children}) {
 
     return (
         <div className={`fixed inset-0 flex flex-col justify-center items-center transition-colors ${open ? "visible bg-black/20" : "invisible"}`}>
-            <button className="py-1 px-2 border border-neutral-200 rounded-md text-gray-400 bg-white mb-2" onClick={onClose}>
-                    close
-                </button>
-            <div className={`max-h-[70vh] overflow-auto bg-white roudned-lg shadow p-6 transition all max-w-md overflow-auto ${open ? "scale-100 opacity-100" : "scale-110 opacity-0"}`}>
+            
+            <div className={`max-h-[45vh] overflow-auto bg-white roudned-lg shadow p-6 transition all max-w-md overflow-auto ${open ? "scale-100 opacity-100" : "scale-110 opacity-0"}`}>
                 <div className="flex flex-col">
                     {children}
                 </div>
-                
             </div>
+            <button className="py-1 px-2 border border-neutral-200 rounded-md text-gray-400 bg-white mt-2" onClick={onClose}>
+                    close
+            </button>
         </div>
     )
 };
