5f9467a1f6fe4872378d1b55907eadeaac32dc17
cedric-castelino <cedric0703@gmail.com>
2025-04-12 22:26:49 +1000

    Add styling to the login page and setup the daisyui library

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-import Button from 'react-bootstrap/Button'
-
 function Login ({ successJob, token,}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -33,14 +31,19 @@ function Login ({ successJob, token,}) {
   };
 
   return (
-    <>
-      <section className="register-page">
-        <h1>Login</h1>
-        Email: <input value={email} onChange={e => setEmail(e.target.value)} type="text"/> <br/> 
-        password: <input value={password} onChange={e => setPassword(e.target.value)} type="text"/> <br/> 
-        <Button onClick={login} variant='primary'>Login</Button>
-      </section>
-    </>
+    <div data-theme="nord" className="flex items-center justify-center min-h-screen bg-gray-100">
+      <fieldset className="fieldset w-xs bg-white border border-base-300 p-4 rounded-box">
+        <legend className="fieldset-legend text-black">Login</legend>
+
+        <label className="fieldset-label text-black">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="fieldset-label text-black">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
+    </div>
   )
 }
 
