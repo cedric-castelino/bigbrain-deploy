9e86f4853c0d2dee70b17b6528b9b50cfab2c5c5
cedric-castelino <cedric0703@gmail.com>
2025-04-15 18:25:09 +1000

    Fix the indentation in the first half of the Session.jsx file to match linting standards

--- frontend/src/Session.jsx
+++ frontend/src/Session.jsx
@@ -4,211 +4,211 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Session = ({ token, setActiveStatus }) => {
-    const linkedSession = useParams();
-    const navigate = useNavigate();
+  const linkedSession = useParams();
+  const navigate = useNavigate();
 
-    const [localActiveStatus, setLocalActiveStatus] = useState(localStorage.getItem('activeStatus'));
-    const [localActiveGameId, setLocalActiveGameId] = useState(localStorage.getItem('activeGameId'));
-    const [localSessionId, setLocalSessionId] = useState(localStorage.getItem('sessionId'));
-    const [numberOfQuestions, setNumberOfQuestions] = useState(parseInt(localStorage.getItem('NumberOfQuestions')));
-    const [currentQuestionPosition, setCurrentQuestionPosition] = useState(parseInt(localStorage.getItem('currentQuestionPosition')));
-    const [gameState, setGameState] = useState(localStorage.getItem("gameState"));
-    const [hasResults, setHasResults] = useState(false);
-    const [questions, setQuestions] = useState([]);
-    const [questionTimer, setQuestionTimer] = useState(localStorage.getItem("questionTimer"));
+  const [localActiveStatus, setLocalActiveStatus] = useState(localStorage.getItem('activeStatus'));
+  const [localActiveGameId, setLocalActiveGameId] = useState(localStorage.getItem('activeGameId'));
+  const [localSessionId, setLocalSessionId] = useState(localStorage.getItem('sessionId'));
+  const [numberOfQuestions, setNumberOfQuestions] = useState(parseInt(localStorage.getItem('NumberOfQuestions')));
+  const [currentQuestionPosition, setCurrentQuestionPosition] = useState(parseInt(localStorage.getItem('currentQuestionPosition')));
+  const [gameState, setGameState] = useState(localStorage.getItem("gameState"));
+  const [hasResults, setHasResults] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [questionTimer, setQuestionTimer] = useState(localStorage.getItem("questionTimer"));
 
-    const activeStatus = localStorage.getItem('activeStatus');
-    const activeGameId = localStorage.getItem('activeGameId');
-    const sessionId = localStorage.getItem('sessionId');
-    const localToken = localStorage.getItem('token');
-    const localGameState = localStorage.getItem('gameState');
-    const localCurrentQuestionPosition = parseInt(localStorage.getItem('currentQuestionPosition'));
-    const localNumberOfQuestions = parseInt(localStorage.getItem('NumberOfQuestions'));
+  const activeStatus = localStorage.getItem('activeStatus');
+  const activeGameId = localStorage.getItem('activeGameId');
+  const sessionId = localStorage.getItem('sessionId');
+  const localToken = localStorage.getItem('token');
+  const localGameState = localStorage.getItem('gameState');
+  const localCurrentQuestionPosition = parseInt(localStorage.getItem('currentQuestionPosition'));
+  const localNumberOfQuestions = parseInt(localStorage.getItem('NumberOfQuestions'));
 
-    useEffect(() => {
-        if (questionTimer <= 0) return;
+  useEffect(() => {
+    if (questionTimer <= 0) return;
     
-        const intervalId = setInterval(() => {
-          setQuestionTimer((prev) => prev - 1);
-        }, 1000);
+    const intervalId = setInterval(() => {
+      setQuestionTimer((prev) => prev - 1);
+    }, 1000);
     
-        // Cleanup when component unmounts or timer ends
-        return () => clearInterval(intervalId);
-      }, [questionTimer]);
+    // Cleanup when component unmounts or timer ends
+    return () => clearInterval(intervalId);
+  }, [questionTimer]);
 
-    useEffect(() => {
-        const checkResults = async () => {
-            // Only check for results if no active session is running
-            if (!activeStatus) {
-                const success = await getResults(localToken);
-                setHasResults(success);
-            }
-        };
-        checkResults();
-    }, [localToken, activeStatus]);  // Re-check when the activeStatus changes
+  useEffect(() => {
+    const checkResults = async () => {
+      // Only check for results if no active session is running
+      if (!activeStatus) {
+        const success = await getResults(localToken);
+        setHasResults(success);
+      }
+    };
+    checkResults();
+  }, [localToken, activeStatus]);  // Re-check when the activeStatus changes
 
-    useEffect(() => {
-        setLocalActiveStatus(localStorage.getItem('activeStatus'));
-        setLocalActiveGameId(localStorage.getItem('activeGameId'));
-        setLocalSessionId(localStorage.getItem('sessionId'));
-        setCurrentQuestionPosition(localCurrentQuestionPosition)
-        setNumberOfQuestions(localNumberOfQuestions)
-        setGameState(localGameState)
+  useEffect(() => {
+    setLocalActiveStatus(localStorage.getItem('activeStatus'));
+    setLocalActiveGameId(localStorage.getItem('activeGameId'));
+    setLocalSessionId(localStorage.getItem('sessionId'));
+    setCurrentQuestionPosition(localCurrentQuestionPosition)
+    setNumberOfQuestions(localNumberOfQuestions)
+    setGameState(localGameState)
 
-    }, []);
+  }, []);
 
-    // Update gameState based on currentQuestionPosition
-    useEffect(() => {
-        if (currentQuestionPosition === -1) {
-            setGameState("waitForPlayersJoin");
-        } else if (currentQuestionPosition === 0) {
-            setGameState("displayQuestions");
-        } else if (currentQuestionPosition + 1 > numberOfQuestions) {
-            localStorage.removeItem('activeStatus');
-            localStorage.removeItem('activeGameId');
-            localStorage.removeItem('sessionId');
-            localStorage.removeItem('gameState');
-            localStorage.removeItem('currentQuestionPosition');
-            localStorage.removeItem('localNumberOfQuestions');
-            setGameState("results");
-        }
-    }, [currentQuestionPosition]);
+  // Update gameState based on currentQuestionPosition
+  useEffect(() => {
+    if (currentQuestionPosition === -1) {
+      setGameState("waitForPlayersJoin");
+    } else if (currentQuestionPosition === 0) {
+      setGameState("displayQuestions");
+    } else if (currentQuestionPosition + 1 > numberOfQuestions) {
+      localStorage.removeItem('activeStatus');
+      localStorage.removeItem('activeGameId');
+      localStorage.removeItem('sessionId');
+      localStorage.removeItem('gameState');
+      localStorage.removeItem('currentQuestionPosition');
+      localStorage.removeItem('localNumberOfQuestions');
+      setGameState("results");
+    }
+  }, [currentQuestionPosition]);
 
-    const renderGameContent = () => {
-        if (gameState === "waitForPlayersJoin" && hasResults) {
-            setGameState("results");
-        }
+  const renderGameContent = () => {
+    if (gameState === "waitForPlayersJoin" && hasResults) {
+      setGameState("results");
+    }
 
-        switch(gameState) {
-            case "waitForPlayersJoin":
-                localStorage.setItem('gameState', 'waitForPlayersJoin');
-                return (<p>Waiting for players to connect</p>)
-            case "displayQuestions":
-                localStorage.setItem('gameState', 'displayQuestions');
-                return (
-                    <div>
-                        <p>Question position: {currentQuestionPosition + 1} / {numberOfQuestions} </p>
-                        <div>
-                            {questionTimer > 0
-                                ? `Duration: ${questionTimer}s`
-                                : "Question is finished"}
+    switch(gameState) {
+    case "waitForPlayersJoin":
+      localStorage.setItem('gameState', 'waitForPlayersJoin');
+      return (<p>Waiting for players to connect</p>)
+    case "displayQuestions":
+      localStorage.setItem('gameState', 'displayQuestions');
+      return (
+        <div>
+          <p>Question position: {currentQuestionPosition + 1} / {numberOfQuestions} </p>
+          <div>
+            {questionTimer > 0
+              ? `Duration: ${questionTimer}s`
+              : "Question is finished"}
 
-                            {questions.length > 0 && currentQuestionPosition >= 0 && currentQuestionPosition < questions.length ? (
-                                <ul>
-                                    <li>
+            {questions.length > 0 && currentQuestionPosition >= 0 && currentQuestionPosition < questions.length ? (
+              <ul>
+                <li>
                                         Question: {questions[currentQuestionPosition].question}
-                                    </li>
-                                    <li>
+                </li>
+                <li>
                                         Option A: {questions[currentQuestionPosition].options.optionA}
-                                    </li>
-                                    <li>
+                </li>
+                <li>
                                         Option B: {questions[currentQuestionPosition].options.optionB || ""}
-                                    </li>
-                                    <li>
+                </li>
+                <li>
                                         Option C: {questions[currentQuestionPosition].options.optionC || ""}
-                                    </li>
-                                    <li>
+                </li>
+                <li>
                                         Option D: {questions[currentQuestionPosition].options.optionD || ""}
-                                    </li>
-                                </ul>
-                            ) : (
-                                <p>Loading questions...</p>
-                            )}
+                </li>
+              </ul>
+            ) : (
+              <p>Loading questions...</p>
+            )}
 
                                 
-                        </div>
-                    </div>
-                )
-            case "results":
-                localStorage.setItem('gameState', 'results');
-                return (
-                    <div>
-                        <p>Results screen</p>
-                        <h1>data</h1>
-                        <h1>data</h1>
-                        <h1>data</h1>
-                    </div>
+          </div>
+        </div>
+      )
+    case "results":
+      localStorage.setItem('gameState', 'results');
+      return (
+        <div>
+          <p>Results screen</p>
+          <h1>data</h1>
+          <h1>data</h1>
+          <h1>data</h1>
+        </div>
                 
-            )
+      )
+    }
+  }
+
+  const endGameFunctionality = async () => {
+    localStorage.removeItem('activeStatus');
+    localStorage.removeItem('activeGameId');
+    localStorage.removeItem('sessionId');
+    localStorage.removeItem('gameState');
+    localStorage.removeItem('currentQuestionPosition');
+    setLocalActiveStatus(null);
+    setLocalActiveGameId(null);
+    setLocalSessionId(null);
+    setActiveStatus(false);
+  }
+
+  const endGameMutate = async (token) => {
+    try {
+      const response = await axios.post(`http://localhost:5005/admin/game/${activeGameId}/mutate`, {
+        mutationType: "END"
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
         }
+      });
+      endGameFunctionality()
+    } catch (err) {
+      alert(err.response.data.error);
     }
+  }
 
-    const endGameFunctionality = async () => {
-        localStorage.removeItem('activeStatus');
-        localStorage.removeItem('activeGameId');
-        localStorage.removeItem('sessionId');
-        localStorage.removeItem('gameState');
-        localStorage.removeItem('currentQuestionPosition');
-        setLocalActiveStatus(null);
-        setLocalActiveGameId(null);
-        setLocalSessionId(null);
-        setActiveStatus(false);
-    }
+  const advanceGame = async (token) => {
 
-    const endGameMutate = async (token) => {
-        try {
-            const response = await axios.post(`http://localhost:5005/admin/game/${activeGameId}/mutate`, {
-                mutationType: "END"
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                }
-            });
-            endGameFunctionality()
-        } catch (err) {
-            alert(err.response.data.error);
+    try {
+      const response = await axios.post(`http://localhost:5005/admin/game/${activeGameId}/mutate`, {
+        mutationType: "ADVANCE"
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
         }
-    }
-
-    const advanceGame = async (token) => {
-
-        try {
-            const response = await axios.post(`http://localhost:5005/admin/game/${activeGameId}/mutate`, {
-                mutationType: "ADVANCE"
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                }
-            });
+      });
             
-            getStatus(token);
-            const newPosition = response.data.data.position;
-            localStorage.setItem('currentQuestionPosition', newPosition.toString())
-            setCurrentQuestionPosition(newPosition);
-            if (newPosition === numberOfQuestions) {
-                endGameFunctionality()
-            }
+      getStatus(token);
+      const newPosition = response.data.data.position;
+      localStorage.setItem('currentQuestionPosition', newPosition.toString())
+      setCurrentQuestionPosition(newPosition);
+      if (newPosition === numberOfQuestions) {
+        endGameFunctionality()
+      }
             
-        } catch (err) {
-            alert(err.response.data.error);
-        }
+    } catch (err) {
+      alert(err.response.data.error);
     }
+  }
 
-    const getStatus = async (token) => {
-        try {
-            const response = await axios.get(`http://localhost:5005/admin/session/${sessionId}/status`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-                params: {  // If you need to send the sessionId as a query parameter
-                    sessionid: sessionId
-                }
-            })
-            const lngth = response.data.results.questions.length;
-            setQuestions(response.data.results.questions)
-            setNumberOfQuestions(lngth);
-            localStorage.setItem('NumberOfQuestions', lngth.toString());
-            if (response && 
+  const getStatus = async (token) => {
+    try {
+      const response = await axios.get(`http://localhost:5005/admin/session/${sessionId}/status`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+        params: {  // If you need to send the sessionId as a query parameter
+          sessionid: sessionId
+        }
+      })
+      const lngth = response.data.results.questions.length;
+      setQuestions(response.data.results.questions)
+      setNumberOfQuestions(lngth);
+      localStorage.setItem('NumberOfQuestions', lngth.toString());
+      if (response && 
                 localCurrentQuestionPosition + 1 < lngth && 
                 localCurrentQuestionPosition >= -1) {
-                setQuestionTimer(response.data.results.questions[localCurrentQuestionPosition + 1].duration);
-                localStorage.setItem('questionTimer', response.data.results.questions[localCurrentQuestionPosition + 1].duration)
-            }
+        setQuestionTimer(response.data.results.questions[localCurrentQuestionPosition + 1].duration);
+        localStorage.setItem('questionTimer', response.data.results.questions[localCurrentQuestionPosition + 1].duration)
+      }
             
-        } catch (err) {
-            alert(err.response.data.error);
-        }
+    } catch (err) {
+      alert(err.response.data.error);
     }
+  }
 
     const getResults = async (token) => {
         try {
