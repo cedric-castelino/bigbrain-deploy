a841db0890206f3b5b92f25e13088e57cceae3f2
cedric-castelino <cedric0703@gmail.com>
2025-04-15 18:23:20 +1000

    Fix the indentation in the rest  of the EditGame.jsx file to match linting standards

--- frontend/src/EditGame.jsx
+++ frontend/src/EditGame.jsx
@@ -205,211 +205,211 @@ const EditGame = ({ token }) => {
       }
     }
       
-        games[games.findIndex(g => g.id === game.id)] = game;
-        try {
-          await axios.put('http://localhost:5005/admin/games', {
-            games: games
-          }, {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          });
+    games[games.findIndex(g => g.id === game.id)] = game;
+    try {
+      await axios.put('http://localhost:5005/admin/games', {
+        games: games
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
     
-          getGameData(token);
-          setDuration('');
-          setQuestion('');
-          setOptionA('');
-          setOptionB('');
-          setOptionC('');
-          setOptionD('');
-          setcorrectAnswer('');
-          setquestionError('');
-          return true;
-        } catch (err) {
-          console.log(err);
-          setquestionError(err.response.data.error);
-          return false;
-        }
+      getGameData(token);
+      setDuration('');
+      setQuestion('');
+      setOptionA('');
+      setOptionB('');
+      setOptionC('');
+      setOptionD('');
+      setcorrectAnswer('');
+      setquestionError('');
+      return true;
+    } catch (err) {
+      console.log(err);
+      setquestionError(err.response.data.error);
+      return false;
+    }
+  }
+
+  const resetFileInput = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.value = null; 
+    }
+  }
+
+  const getTotalDuration = (questions) => {
+    let totalDuration = 0;
+    for (const question of questions) {
+      totalDuration += Number(question.duration);
     }
 
-    const resetFileInput = () => {
-        if (fileInputRef.current) {
-            fileInputRef.current.value = null; 
-        }
+    const minutes = Math.floor(totalDuration / 60);
+    const seconds = totalDuration % 60;
+    const parts = [];
+
+    if (minutes > 0) {
+      parts.push(`${minutes} minute${minutes !== 1 ? 's' : ''}`);
+    }
+    if (seconds > 0 || parts.length === 0) {
+      parts.push(`${seconds} second${seconds !== 1 ? 's' : ''}`);
+    }
+    return parts.join(' and ');
+  }
+
+  const checkDefaultThumbnail = (thumbnail) => {
+    return thumbnail === defaultImg;
+  };
+
+  const deleteQuestion = async (id) => {
+    let currentQuestions = game.questions.filter(q => q.id !== id);
+    let count = 1
+    for (const question of currentQuestions) {
+      question.id = count;
+      count += 1;
     }
 
-    const getTotalDuration = (questions) => {
-        let totalDuration = 0;
-        for (const question of questions) {
-            totalDuration += Number(question.duration);
-        }
+    game.questions = currentQuestions;
+    games[games.findIndex(g => g.id === game.id)] = game;
 
-        const minutes = Math.floor(totalDuration / 60);
-        const seconds = totalDuration % 60;
-        const parts = [];
-
-        if (minutes > 0) {
-            parts.push(`${minutes} minute${minutes !== 1 ? 's' : ''}`);
-        }
-        if (seconds > 0 || parts.length === 0) {
-            parts.push(`${seconds} second${seconds !== 1 ? 's' : ''}`);
-        }
-        return parts.join(' and ');
-    }
-
-    const checkDefaultThumbnail = (thumbnail) => {
-        return thumbnail === defaultImg;
-    };
-
-    const deleteQuestion = async (id) => {
-        let currentQuestions = game.questions.filter(q => q.id !== id);
-        let count = 1
-        for (const question of currentQuestions) {
-            question.id = count;
-            count += 1;
-        }
-
-        game.questions = currentQuestions;
-        games[games.findIndex(g => g.id === game.id)] = game;
-
-        await axios.put('http://localhost:5005/admin/games', {
-          games: games
-        }, {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
+    await axios.put('http://localhost:5005/admin/games', {
+      games: games
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
       
-        getGameData(token);
-    };
+    getGameData(token);
+  };
 
-    return (
-        <div className='m-4'>
-            <h1 className="!mb-16 sm:!mb-4">Edit Game</h1>
-            <button onClick={navigate_to_dashboard} className="btn bg-primary hover:!bg-blue-600 text-white absolute top-16 right-6 sm:top-2 sm:right-[8.25rem] sm:btn-lg btn-lg">Dashboard</button>
-            {game.length === 0 ? (
-                <div className='join join-vertical lg:join-horizontal'>
-                    <legend className="text-3xl">Loading</legend>
-                    <span className="ml-3 loading loading-spinner loading-sm"></span>
-                </div>
-            ) : (
-                <div className='w-full h-full'>
-                    <div className="hero bg-base-200 bg-white">
-                        <div className='join join-vertical sm:join-horizontal w-full'>
-                        <img
-                            src={game.thumbnail}
-                            className="rounded-lg shadow-2xl w-[20%] m-6 mr-0" />
-                        <div className="p-6">
-                            <h1 className="text-5xl font-bold">{game.name}</h1>
-                            <p className="py-0 mb-2">
-                            Number of Questions: {game.questions.length}
-                            </p>
-                            <p className="py-0">
-                            Total Duration: {getTotalDuration(game.questions)}
-                            </p>
-                            <div className='join join-vertical sm:join-horizontal gap-2'>
-                                <button className="btn btn-primary" onClick={() => setEditPopUp(true)}>Edit Game Info</button>
-                                {!checkDefaultThumbnail(game.thumbnail) && (
-                                    <button className="btn btn-primary" onClick={() => editGame(true)}>Reset Thumbnail</button>
-                                )}
-                            </div>
-                            <CreateGameModal
-                                open={editPopUp}
-                                onClose={() => {
-                                setEditPopUp(false);
-                                setName(''); 
-                                setThumbnail(''); 
-                                resetFileInput();
-                                seteditGameError(''); 
-                                }}
-                                name={name}
-                                setName={setName}
-                                handleFileChange={handleFileChange}
-                                onCreate={async () => {
-                                const success = await editGame(false);
-                                if (success) {
-                                    setEditPopUp(false); 
-                                }
-                                }}
-                                error={editGameError}
-                                editing={false}
-                                fileInputRef={fileInputRef}
-                            />
-                        </div>
-                        </div>
-                    </div>
-                    <button className="btn btn-primary mt-4" 
-                        onClick={() => {
-                            setCreateQPopUp(true);
-                            setEditQuestion(false);
-                        }}>Add Question</button>
-                    <CreateQuestionModal
-                        open={createQPopuUp}
-                        onClose={() => {
-                        setCreateQPopUp(false);
-                        setDuration('');
-                        setQuestion('');
-                        setOptionA('');
-                        setOptionB('');
-                        setOptionC('');
-                        setOptionD('');
-                        setcorrectAnswer('');
-                        setquestionError('');
-                        }}
-                        onCreate={async () => {
-                        const success = await addQuestion();
-                        if (success) {
-                            setCreateQPopUp(false); 
-                        }
-                        }}
-                        error={questionError}
-                        duration={duration}
-                        setDuration={setDuration}
-                        question={question}
-                        setQuestion={setQuestion}
-                        optionA={optionA}
-                        setOptionA={setOptionA}
-                        optionB={optionB}
-                        setOptionB={setOptionB}
-                        optionC={optionC}
-                        setOptionC={setOptionC}
-                        optionD={optionD}
-                        setOptionD={setOptionD}
-                        correctAnswer={correctAnswer}
-                        setcorrectAnswer={setcorrectAnswer}
-                        editing={editQuestion}
-                        onEdit={async () => {
-                            const success = await editQuestionContent(editQuestionID);
-                            if (success) {
-                                setCreateQPopUp(false); 
-                            }
-                            }}
-                    />
-                    {game.questions.length === 0 ? (
-                        <div role="alert" className="alert alert-error mt-6 !bg-red-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <span>No Questions Found.</span>
-                        </div>
-                        ) : (
-                        <div className="grid grid-cols-1 bg-blue-200">
-                            {game.questions.map(question => (
-                            <DisplayQuestions key={question.id}
-                                question={question}
-                                onEdit={() => {
-                                    setEditQuestion(true);
-                                    setEditQuestionID(question.id);
-                                    setCreateQPopUp(true)}}
-                                onDelete={() => {deleteQuestion(question.id)}}
-                            />
-                            ))}
-                        </div>
-                    )}
-                </div>
-            )}
+  return (
+    <div className='m-4'>
+      <h1 className="!mb-16 sm:!mb-4">Edit Game</h1>
+      <button onClick={navigate_to_dashboard} className="btn bg-primary hover:!bg-blue-600 text-white absolute top-16 right-6 sm:top-2 sm:right-[8.25rem] sm:btn-lg btn-lg">Dashboard</button>
+      {game.length === 0 ? (
+        <div className='join join-vertical lg:join-horizontal'>
+          <legend className="text-3xl">Loading</legend>
+          <span className="ml-3 loading loading-spinner loading-sm"></span>
         </div>
-    );
+      ) : (
+        <div className='w-full h-full'>
+          <div className="hero bg-base-200 bg-white">
+            <div className='join join-vertical sm:join-horizontal w-full'>
+              <img
+                src={game.thumbnail}
+                className="rounded-lg shadow-2xl w-[20%] m-6 mr-0" />
+              <div className="p-6">
+                <h1 className="text-5xl font-bold">{game.name}</h1>
+                <p className="py-0 mb-2">
+                            Number of Questions: {game.questions.length}
+                </p>
+                <p className="py-0">
+                            Total Duration: {getTotalDuration(game.questions)}
+                </p>
+                <div className='join join-vertical sm:join-horizontal gap-2'>
+                  <button className="btn btn-primary" onClick={() => setEditPopUp(true)}>Edit Game Info</button>
+                  {!checkDefaultThumbnail(game.thumbnail) && (
+                    <button className="btn btn-primary" onClick={() => editGame(true)}>Reset Thumbnail</button>
+                  )}
+                </div>
+                <CreateGameModal
+                  open={editPopUp}
+                  onClose={() => {
+                    setEditPopUp(false);
+                    setName(''); 
+                    setThumbnail(''); 
+                    resetFileInput();
+                    seteditGameError(''); 
+                  }}
+                  name={name}
+                  setName={setName}
+                  handleFileChange={handleFileChange}
+                  onCreate={async () => {
+                    const success = await editGame(false);
+                    if (success) {
+                      setEditPopUp(false); 
+                    }
+                  }}
+                  error={editGameError}
+                  editing={false}
+                  fileInputRef={fileInputRef}
+                />
+              </div>
+            </div>
+          </div>
+          <button className="btn btn-primary mt-4" 
+            onClick={() => {
+              setCreateQPopUp(true);
+              setEditQuestion(false);
+            }}>Add Question</button>
+          <CreateQuestionModal
+            open={createQPopuUp}
+            onClose={() => {
+              setCreateQPopUp(false);
+              setDuration('');
+              setQuestion('');
+              setOptionA('');
+              setOptionB('');
+              setOptionC('');
+              setOptionD('');
+              setcorrectAnswer('');
+              setquestionError('');
+            }}
+            onCreate={async () => {
+              const success = await addQuestion();
+              if (success) {
+                setCreateQPopUp(false); 
+              }
+            }}
+            error={questionError}
+            duration={duration}
+            setDuration={setDuration}
+            question={question}
+            setQuestion={setQuestion}
+            optionA={optionA}
+            setOptionA={setOptionA}
+            optionB={optionB}
+            setOptionB={setOptionB}
+            optionC={optionC}
+            setOptionC={setOptionC}
+            optionD={optionD}
+            setOptionD={setOptionD}
+            correctAnswer={correctAnswer}
+            setcorrectAnswer={setcorrectAnswer}
+            editing={editQuestion}
+            onEdit={async () => {
+              const success = await editQuestionContent(editQuestionID);
+              if (success) {
+                setCreateQPopUp(false); 
+              }
+            }}
+          />
+          {game.questions.length === 0 ? (
+            <div role="alert" className="alert alert-error mt-6 !bg-red-200">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+              <span>No Questions Found.</span>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 bg-blue-200">
+              {game.questions.map(question => (
+                <DisplayQuestions key={question.id}
+                  question={question}
+                  onEdit={() => {
+                    setEditQuestion(true);
+                    setEditQuestionID(question.id);
+                    setCreateQPopUp(true)}}
+                  onDelete={() => {deleteQuestion(question.id)}}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EditGame;
