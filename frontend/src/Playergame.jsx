6604a2abc2691483e0d56e489230aeb44b58cacb
Geoffrey Mok <z5417523@ad.unsw.edu.au>
2025-04-16 23:18:28 +1000

    sessionId is either empty string or prepopulatedSessionID which can change based on the input

--- frontend/src/Playergame.jsx
+++ frontend/src/Playergame.jsx
@@ -5,15 +5,16 @@ import { useNavigate, Link, useParams } from 'react-router-dom';
 function Playergame ({ token }) {
 
   // Collects user input for the login form
-  const navigate = useNavigate();
-  const [sessionId, setSessionId] = useState('');
-  const [name, setName] = useState('');
   const prePopulatedSessionId = useParams().sessionId;
+  const navigate = useNavigate();
+  const [sessionId, setSessionId] = useState(prePopulatedSessionId || '');
+  const [name, setName] = useState('');
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-200">
         <form className='flex flex-col gap-3'>
             <label type="text" className='mr-4'>Session ID</label>
-            <input value={prePopulatedSessionId} onChange={e => setSessionId(e.target.value)} type='number' className='bg-white'></input>
+            <input value={sessionId} onChange={e => setSessionId(e.target.value)} type='number' className='bg-white'></input>
             <label type="text" className='mr-4'>Name</label>
             <input value={name} onChange={e => setName(e.target.value)} type='text' className='bg-white'></input>
             <button className=' btn btn-primary'>
